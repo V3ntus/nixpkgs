9878cd4e196424fe497303d20ef68fe4bfc578e8
V3ntus <29584664+V3ntus@users.noreply.github.com>
2024-04-13 20:27:56 -0500

    wip: wazuh agent service module

---
+++ nixos/modules/services/security/wazuh-agent.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  wazuhUser = "wazuh";
+  wazuhGroup = wazuhUser;
+  stateDir = "/var/ossec";
+  cfg = config.services.wazuh;
+  pkg = pkgs.wazuh;
+in {
+  options = {
+    services.wazuh = {
+      agent = {
+        enable = mkEnableOption (lib.mdDoc "Wazuh agent");
+
+        # TODO: mkOption -> XML for ossec.conf
+      };
+    };
+  };
+
+  config = mkIf ( cfg.agent.enable ) {
+    environment.systemPackages = [ pkg ];
+
+    users.users.${ wazuhUser } = {
+      uid = config.ids.uids.wazuh;
+      group = wazuhGroup;
+      description = "Wazuh daemon user";
+      home = stateDir;
+    };
+
+    users.groups.${ wazuhGroup } = {
+      gid = configs.ids.gids.wazuh;
+    };
+
+    systemd.services.wazuh-agent = mkIf cfg.agent.enable {
+      description = "Wazuh agent";
+      wants = [ "network-online.target" ];
+      after = [ "network.target" "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      preStart = ''
+        mkdir -m 0750 -p ${stateDir}
+        cp -rf ${pkg}/* ${stateDir}
+
+        find ${stateDir} -type f -exec chmod 644 {}
+        find ${stateDir} -type d -exec chmod 750 {}
+        chmod u+x ${stateDir}\{bin,active-response/bin\}/*
+        chown -R ${wazuhUser}:${wazuhGroup} ${stateDir}
+      '';
+
+      serviceConfig = {
+        Type = "forking";
+        ExecStart = "env ${pkg}/bin/wazuh-control start";
+        ExecStop = "env ${pkg}/bin/wazuh-control stop";
+        ExecReload = "env ${pkg}/bin/wazuh-control reload";
+        KillMode = "process";
+        RemainAfterExit = "yes";
+      };
+    };
+  };
+}
